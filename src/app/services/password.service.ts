66ac43813cfd1cf24584a7b44dc6197a1d9578ae
kylianSalomon <kylian.salomon@orange.com>
2021-04-01 16:04:04 +0200

    Add httpParams

--- src/app/services/password.service.ts
+++ src/app/services/password.service.ts
@@ -1,24 +1,35 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders} from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable, pipe } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { GeneratedPassword } from '../models/GeneratedPassword'
+import { GeneratedPassword } from '../models/GeneratedPassword';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PasswordService {
-  url:string = "https://api.happi.dev/v1/generate-password"
-  key:string = "?apikey=9521e215U92Y1HuZTuRNvb8echm9kPliqYz0K8CgUXjLJBs9wNqOQnw9"
+  url: string = 'https://api.happi.dev/v1/generate-password';
+  key: string =
+    '?apikey=9521e215U92Y1HuZTuRNvb8echm9kPliqYz0K8CgUXjLJBs9wNqOQnw9';
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   //get password with given options
-  getPassword(lenght:string,num:string,upper:string,symbols:string):Observable<string[]>{
-    return this.http.get<GeneratedPassword>(`${this.url}${this.key}&limit=1&length=${lenght}&num=${num}&upper=${upper}&symbols=${symbols}`)
-                    .pipe(
-                      map((data: GeneratedPassword) => data.passwords)
-                    );
+  getPassword(
+    length: string,
+    num: Boolean,
+    upper: Boolean,
+    symbols: Boolean
+  ): Observable<string[]> {
+    const params = new HttpParams()
+      .set('limit', '1')
+      .set('length', length)
+      .set('num', num ? '1' : '0')
+      .set('upper', upper ? '1' : '0')
+      .set('symbols', symbols ? '1' : '0');
+
+    return this.http
+      .get<GeneratedPassword>(`${this.url}${this.key}`, { params })
+      .pipe(map((data: GeneratedPassword) => data.passwords));
   }
-  
 }
