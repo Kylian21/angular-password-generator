d732e737cb390cec473481b075d901969114f550
kylianSalomon <kylian.salomon@orange.com>
2021-04-09 16:40:22 +0200

    Remove useless methode and initializer for GeneratedPasswordError

--- src/app/models/GeneratedPasswordError.ts
+++ src/app/models/GeneratedPasswordError.ts
@@ -1,9 +1,5 @@
 export class GeneratedPasswordError extends Error {
-  constructor(message: string, status?: number) {
-    super((message = message));
-  }
-
-  get message(): string {
-    return super.message;
+  constructor(message: string) {
+    super(message);
   }
 }
