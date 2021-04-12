e4c329bfade16c8d6e73fec9cfba089fba4263ed
kylianSalomon <kylian.salomon@orange.com>
2021-04-12 11:13:35 +0200

    Remove clickEmitter observable by (click) event

--- src/app/components/saved-passwords/saved-passwords.component.ts
+++ src/app/components/saved-passwords/saved-passwords.component.ts
@@ -1,7 +1,5 @@
 import { Component, ElementRef } from '@angular/core';
 import { LocalStorageService } from '../../services/localStorage/local-storage.service';
-import { Observable, fromEvent, of } from 'rxjs';
-import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-saved-passwords',
@@ -9,16 +7,17 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./saved-passwords.component.css'],
 })
 export class SavedPasswordsComponent {
-  readonly clickEmmiter: Observable<string> = fromEvent(
-    this.eRef.nativeElement,
-    'click'
-  ).pipe(map((x) => ((x as Event).target as Element).id));
   passwords(): string[] {
     return this.storageService.getPasswords();
   }
-
+  itemClicked: string = '';
   constructor(
     private storageService: LocalStorageService,
     private eRef: ElementRef
   ) {}
+
+  onClick(item: string) {
+    this.itemClicked = item;
+    console.log(item);
+  }
 }
